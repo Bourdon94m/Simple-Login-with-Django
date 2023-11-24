7ccbffc77e74f4073c83653d41391ab1a99d94e1
Bourdon94m <105060610+Bourdon94m@users.noreply.github.com>
2023-11-24 20:03:23 +0100

    new login
    New UI login made.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [r"C:\Users\mattl\OneDrive\Bureau\Login\src\login\templates", r"C:\Users\mattl\OneDrive\Bureau\Login\src\pages\templates",r"C:\Users\mattl\OneDrive\Bureau\Login\src\pages\templates\for_portfolio", r"login/templates/auth"],
+        'DIRS': [r"C:\Users\mattl\OneDrive\Bureau\Simple-Login-with-Django\login\templates\auth", r"C:\Users\mattl\OneDrive\Bureau\Simple-Login-with-Django\pages\templates\for_portfolio"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
